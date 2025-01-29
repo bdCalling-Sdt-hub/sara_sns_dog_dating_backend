bee2656961d79722e0d66af8eed389af6d9e5ea1
Rasel Chowdhury <87080744+rasel-chowdhury1@users.noreply.github.com>
2025-01-29 11:14:31 +0600

    Update cors origin of app.ts file

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
-    // origin: 'localhost:3000',
+   // origin: true,
+    origin: 'https://woofspot.net',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
