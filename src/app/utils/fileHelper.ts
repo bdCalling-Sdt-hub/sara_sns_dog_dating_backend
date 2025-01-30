3935319269353d3325c4680ee8b42838158b941e
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2025-01-30 10:55:37 +0600

    updated

--- src/app/utils/fileHelper.ts
+++ src/app/utils/fileHelper.ts
@@ -3,11 +3,11 @@ import util from 'util';
 const unlinkSync = util.promisify(fs.unlink);
 export const deleteFile = async (path: string) => {
   try {
-    if (fs.existsSync(`../public/${path}`)) {
-      await unlinkSync(`../public/${path}`);
-    } else {
-      console.log('not found');
-    }
+    // if (fs.existsSync(`../public/${path}`)) {
+    //   await unlinkSync(`../public/${path}`);
+    // } else {
+    //   console.log('not found');
+    // }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     throw new Error(`Error deleting file: ${err.message}`);
